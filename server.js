b4f994d1973fece0a6aecf19ea416c0de1642212
lalluV <87778146+lalluV@users.noreply.github.com>
2025-06-03 14:27:56 +0530

    domain connected

--- server.js
+++ server.js
@@ -59,6 +59,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
